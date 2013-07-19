755e36ec0f16a12ab4942ebb646b3fa809a1b1ba
Kate Hudson <k88hudson@gmail.com>
2013-07-18 20:50:46 -0400

    codemirror and parsing stuff

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var nunjucks = require('nunjucks-middleware');
 var routes = require('./routes');
 var app = express();
 var http = require('http');
+var stylus = require('stylus');
+var nib = require('nib');
+
+// Stylus
+function compileStylus(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('compress', false)
+    .set('force', true)
+    .use(nib());
+}
 
 app.configure(function(){
   app.set('port', process.env.PORT || 8888);
@@ -12,10 +23,14 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(nunjucks({
-    src: "/views",
-    endpoint: "/js/template.js",
+    src: '/views',
+    endpoint: '/js/template.js',
     debug: true,
     express: app
+  }));
+  app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compileStylus
   }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
@@ -25,6 +40,8 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+app.use('/codemirror', express.static(__dirname + '/node_modules/codemirror'));
+app.use('/gfm', express.static(__dirname + '/node_modules/github-flavored-markdown'));
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
