81dca8f4b93050c6bbb2beffde489171f5355331
Kate Hudson <k88hudson@gmail.com>
2013-07-19 00:25:46 -0400

    replace [gif]

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,13 @@ exports.gif = function(req, res){
   var whimsyPad = "http://firefox-ux.etherpad.mozilla.org/ep/pad/export/thumbnail-gifs/latest?";
 
   request.get(whimsyPad, function(err, resp, body) {
-    var gifs = body.split('\n');
+    //var gifs = body.split('\n');
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    var gifs = body.match(regex);
+    console.log(gifs.length);
     var random = Math.floor(Math.random() * gifs.length);
-    res.json({url: gifs[random]});
+    res.redirect(gifs[random]);
   });
 };
