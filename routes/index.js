ad2a5a3d677bc9cd2a18ee69ffcf1377dbbf4a0d
Kate Hudson <k88hudson@gmail.com>
2013-07-19 09:18:48 -0400

    gif updates

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,24 @@
-
 var request = require('request');
+var whimsyPad = "http://firefox-ux.etherpad.mozilla.org/ep/pad/export/thumbnail-gifs/latest?";
+var gifs;
+
 
 exports.index = function(req, res){
   res.render('index.html', { title: 'Express' });
 };
 
 exports.gif = function(req, res){
-  var whimsyPad = "http://firefox-ux.etherpad.mozilla.org/ep/pad/export/thumbnail-gifs/latest?";
-
-  request.get(whimsyPad, function(err, resp, body) {
-    //var gifs = body.split('\n');
-    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-    var regex = new RegExp(expression);
-
-    var gifs = body.match(regex);
-    console.log(gifs.length);
-    var random = Math.floor(Math.random() * gifs.length);
-    res.redirect(gifs[random]);
-  });
+  function redirect() {
+    var id = req.query.id || Math.floor(Math.random() * gifs.length);
+    res.redirect(gifs[id]);
+  }
+  if (!gifs) {
+    request.get(whimsyPad, function(err, resp, body) {
+      gifs = body.match(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/g);
+      redirect();
+    });
+  } else {
+    redirect();
+  }
 };
+
